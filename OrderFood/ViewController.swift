0c46398b3188ce8b3c02de21818b3f5492934dd1
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-06-22 22:48:51 +0300

    Initial Commit

---
+++ OrderFood/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OrderFood
+//
+//  Created by Ярослав Любиченко on 22.6.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
