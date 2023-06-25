939972f09736a272e18d4fb9c144131ee048b3e3
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-06-25 20:37:10 +0300

    Create and set TabBar Controller.

---
+++ OrderFood/ViewControlles/MainTabBarController.swift
@@ -0,0 +1,52 @@
+//
+//  MainTabBarController.swift
+//  OrderFood
+//
+//  Created by Ярослав Любиченко on 22.6.2023.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+    
+    private var isSelected = false
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        UITabBar.appearance().backgroundColor = .white
+        createTabBarController()
+    }
+    
+    private func createTabBarController() {
+        viewControllers = [
+            createVC(
+                viewController: MenuViewController(),
+                title: "Меню",
+                image: UIImage(named: "Menu")
+            ),
+            createVC(
+                viewController: ContactsViewController(),
+                title: "Контакты",
+                image: UIImage(named: "Contacts")
+            ),
+            createVC(
+                viewController: ProfileViewController(),
+                title: "Профиль",
+                image: UIImage(systemName: "person.fill")
+            ),
+            createVC(
+                viewController: CartViewController(),
+                title: "Корзина",
+                image: UIImage(named: "Cart")
+            )
+        ]
+    }
+    
+    private func createVC(viewController: UIViewController, title: String, image: UIImage?) -> UIViewController {
+        viewController.tabBarItem.title = title
+        viewController.tabBarItem.image = image
+        
+        return viewController
+    }
+}
