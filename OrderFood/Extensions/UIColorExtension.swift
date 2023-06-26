5841b1faa908bd43eadf7527cc2a8feacf923c10
Snow37 <110422108+Snow37@users.noreply.github.com>
2023-06-26 19:17:11 +0300

    Add extensions for TabBar tintColor.

---
+++ OrderFood/Extensions/UIColorExtension.swift
@@ -0,0 +1,18 @@
+//
+//  UIColorExtension.swift
+//  OrderFood
+//
+//  Created by Ярослав Любиченко on 26.6.2023.
+//
+
+import UIKit
+
+extension UIColor {
+    static var tabBarItemSelected: UIColor {
+        UIColor(red: 0.99, green: 0.23, blue: 0.41, alpha: 1)
+    }
+    
+    static var tabBarColorNotSelected: UIColor {
+        UIColor(red: 0.76, green: 0.77, blue: 0.79, alpha: 1)
+    }
+}
